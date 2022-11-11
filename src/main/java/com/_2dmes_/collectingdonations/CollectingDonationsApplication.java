5d83cb6c79e1ca837c61402e13278ba6bc19a752
Tsygankov-Slava <st200399200399@gmail.com>
2022-11-11 20:12:57 +0300

    init

---
+++ src/main/java/com/_2dmes_/collectingdonations/CollectingDonationsApplication.java
@@ -0,0 +1,13 @@
+package com._2dmes_.collectingdonations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectingDonationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectingDonationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/_2dmes_/collectingdonations/CollectingDonationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com._2dmes_.collectingdonations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectingDonationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
